1581afeebff8b28e364a94191274c9ca1e03449b
JohnEbaid <johnebaid90.gmail.com>
2025-06-24 16:33:31 -0400

    ore updates!

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log("Hello World!");
+
+let myString ="ABc123";
+console.log(myString);
+console.log(typeof myString);
+
+let myNumVar= 123;
+console.log(myNumVar);
+console.log(typeof myNumVar);
+
+console.log(10+ " eggs");
+console.log(10 + 5 +" eggs");
+console.log("eggs " + 10 + 5);
+
+function introduction(name, age){
+    console.log("Hello my name is " + name + ". I am " + "years old.");
+}
+introduction("bob", 71)
+
+
+function school(a,b,c){
+    console.log("Hello my name is " + a + " I go to " + b + " and I am a " + c + " major");
+}
+school ("John", "NYIT", "computer science");
+
+var techClass = ["Sebastian", "Agona", "Karan", "John"];
+console.log(techClass[2]);
+
+
+var sports = ["Basketball","Soccer","Football","Baseball","Hockey","Tennis","Cricket","Pickleball","Ping Pong", "Bowling"];
+console.log(sports[4]);
+console.log(sports[0].length);
+console.log(sports[8].toUpperCase());
